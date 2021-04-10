1ca7d144ac521b7436637fdc785ba1ca4dbd9f9b
FrancescoDiSalesGithub <f.disales@hotmail.com>
2021-04-10 18:17:41 +0200

    add directory argument

--- sample/dirtyops.py
+++ sample/dirtyops.py
@@ -4,28 +4,69 @@ import os
 from graphics import graphics_controller
 import downloaderops
 
-try:    
+try:
 	render = graphics_controller()
+
 	if len(sys.argv) == 1:
 		render.drawBanner()
 		print("Insert at least an argument \n")
 		render.printHelp()
+		os.abort()
 
+	if len(sys.argv) == 2:
+		render.drawBanner()
+		print("Directory argument is missing \n")
+		render.printHelp()
+		os.abort()
+        
+	if len(sys.argv) > 3:
+		render.drawBanner()
+		print("Too much arguments \n")
+		render.printHelp()
+		os.abort()
+                
 	data = sys.argv[1]
+	directory = sys.argv[2]
+	currentdir = os.getcwd()
+		
 	render.drawBanner()
 
 	if data == "deploy":
+		os.chdir(directory)
 		downloaderops.deploy()
+	
+		print("deploy template downloaded in: ")
+		print(directory)
+                
+		os.chdir(currentdir)
 	elif data == "configmap":
+		os.chdir(directory)
 		downloaderops.configmap()
+		
+		print("configmap template downloaded in: ")
+		print(directory)
+                
+		os.chdir(currentdir)
 	elif data == "secret":
+		os.chdir(directory)
 		downloaderops.secret()
+		
+		print("secret template downloaded in: ")
+		print(directory)
+                
+		os.chdir(currentdir)
 	elif data == "service":
+		os.chdir(directory)
 		downloaderops.service()
+		
+		print("service template downloaded in: ")
+		print(directory)
+                
+		os.chdir(currentdir)
 	elif data == "help" or data=="--help" or data=="-h":
 		render.printHelp()
 	else:
 		print("Wrong argument passed \n")
 		render.printHelp()
-except:
-	print("Something bad happened check your internet connection")
+except Exception as e:
+	print(e)
--- sample/graphics.py
+++ sample/graphics.py
@@ -5,13 +5,21 @@ class graphics_controller:
 	def drawBanner(self):
 		print("----------------------------------------------")
 		print("DirtyOps")
-		print("----------------------------------------------")
+		print("---------------------------------------------- \n")
 		
 	def printHelp(self):
+		print("------------------------------------------------")
+		print("Example usage")
+		print("------------------------------------------------ \n")
+		print("example usage for linux users: dirtyops.py deployment /home/user/myproject")
+		print("example usage for windows users: dirtyops.py deployment C:/user/myproject \n \n")
+		print("------------------------------------------------")
+		print("Arguments")
+		print("------------------------------------------------ \n")		
 		print("deployment = downloads a deployment yaml file")
 		print("service = downloads a service yaml file")
 		print("configmap = downloads a configmap yaml file")
 		print("secret = downloads a secret yanl file")
 		print("--help = calls this help screen")
 		print("help = calls this help screen")
-		print("-h = calls this help screen")
+		print("-h = calls this help screen \n")
