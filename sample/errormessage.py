fc4dcd66c11dedee3b8ff21ddbd8b960a9273fb1
Francesco Di Sales <f.disales@hotmail.com>
2021-04-12 17:08:03 +0200

    added content

--- sample/errormessage.py
+++ sample/errormessage.py
@@ -1,3 +1,4 @@
 
-directory_error = ""
-one_argumenter = ""
+directory_error = "Directory argument is missing \n"
+one_argumenter = "Insert at least an argument \n""
+toomuch_argumenter = "Too much arguments \n"
