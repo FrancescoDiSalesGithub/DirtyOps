244eb169dea791896d9064c550cb079fe57d66a2
Francesco Di Sales <f.disales@hotmail.com>
2021-04-12 17:09:08 +0200

    typo

--- sample/errormessage.py
+++ sample/errormessage.py
@@ -1,4 +1,4 @@
 
 directory_error = "Directory argument is missing \n"
-one_argumenter = "Insert at least an argument \n""
+one_argumenter = "Insert at least an argument \n"
 toomuch_argumenter = "Too much arguments \n"
